203d779cd2baf6b41167602c115d6196ee687942
WeZhengTing <2946073318@qq.com>
2022-04-29 22:36:32 +0800

    Insert data into the database

---
+++ 插入数据.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+sqlDB(dbName,"INSERT INTO product VALUES (1,'蒙牛巴氏鲜牛奶','44e7652b-a90e-4328-a89f-74471de7e218','180ml/袋','1.99','3.5');")
+    sqlDB(dbName,"INSERT INTO product VALUES (2,'妃子笑荔枝','397a27fe-2a2a-43c8-bc23-adbdd3a61375','240-250g/份','17.90','19.80');")
+    sqlDB(dbName,"INSERT INTO product VALUES (3,'圣农香煎牛排（黑椒味）','ef88141b-96d3-4d12-839e-433313867936','100g/袋','14.8','14.8');")
+    sqlDB(dbName,"INSERT INTO product VALUES (4,'贝花子青汁牛奶','babf254e-7212-4d1d-bc32-6eeac3867b68','180g/袋','2.5','3.9');")
+    sqlDB(dbName,"INSERT INTO product VALUES (5,'1Cake彩虹瑞士卷','7faf8d9d-0afd-47c7-a211-3f2b17af8371','150g/份','18.8','18.8');")
+    sqlDB(dbName,"INSERT INTO product VALUES (6,'樱桃可可黑森林蛋糕','f61b1b9c-794e-4272-bcbe-7f034f27c06d','4寸','68.0','78.0');")
+    sqlDB(dbName,"INSERT INTO product VALUES (7,'糕守道奶皮白蛋糕','349c0e02-2af3-4174-99ee-662db487c290','400g/箱','16.9','19.9');")
+    sqlDB(dbName,"INSERT INTO product VALUES (8,'1cake抱抱卷','a7831b28-94cd-4a27-802b-fbb5047af20c','110g/盒','8.8','12.8');")
+    sqlDB(dbName,"INSERT INTO product VALUES (9,'友梦虎皮蛋糕（冻）','497432a5-7a3f-422e-b95f-e44aeb236963','200g/份','6.98','12.8');")
+    sqlDB(dbName,"INSERT INTO product VALUES (10,'鲜鸡蛋','6bc4d43c-33de-47a2-a199-6dc719b02380','15枚700g/盒','12.8','13.9');")
+    
